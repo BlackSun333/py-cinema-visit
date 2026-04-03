82e14db19e8921dde0fb143247e67f305b0a037d
Stanislav <staskobetc@gmail.com>
2026-04-03 19:09:21 +0300

    Solution

--- app/people/customer.py
+++ app/people/customer.py
@@ -5,11 +5,3 @@ class Customer:
 
     def watch_movie(self, movie: str) -> None:
         print(f"""{self.name} is watching "{movie}".""")
-
-
-class Cleaner:
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-    def clean_hall(self, hall_number: int) -> None:
-        print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
